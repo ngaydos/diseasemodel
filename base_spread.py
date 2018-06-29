155568bd66729d96a666022101b35752af2e2069
Nicholas Gaydos <ngaydos@gmail.com>
2018-06-29 16:51:39 -0700

    added some notes

--- base_spread.py
+++ base_spread.py
@@ -20,6 +20,7 @@ def simulate(people, num_infect):
     return count_infected/len(people)
 
 def all_infected(people):
+    '''checks to see if all people who are infected have checked'''
     for person in people:
         if person.infected == True:
             if person.checked == True:
@@ -30,6 +31,9 @@ def all_infected(people):
 
 def infect(person, people, num_infect):
     '''
+    takes a person (assumes they are infected) and infects a num_infect number of people from a list of people
+    inputs: person: A Person object, people: A list of Person objects, num_infect: Int to infect
+    outputs: None
     '''
     to_infect = random.sample(people, num_infect)
     for i in to_infect:
