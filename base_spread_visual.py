95e7e9a7352bc6ae5be99d1d9ef7f62ebd9e0ab7
Nicholas Gaydos <ngaydos@gmail.com>
2018-07-03 14:51:52 -0700

    create multitest for graphing multiple instances at the same rate

--- base_spread_visual.py
+++ base_spread_visual.py
@@ -1,9 +1,10 @@
 import random
 from base_spread import Person, all_infected, infect
+import matplotlib.pyplot as plt
 
 def simulate(people, num_infect):
     '''simulates an infection in a group of people. Each person is exposed 1 time to num_infect other people
-    and each person exposed has one chance of catching the disease at a infection_rate chance of exposure.
+    and each person exposed infects other people. This tracks the number of people infected at each generation looped through.
     '''
     infected = []
     random.choice(people).infected = True
@@ -14,3 +15,15 @@ def simulate(people, num_infect):
         count_infected = sum([person.infected for person in people])
         infected.append(count_infected)
     return infected
+
+
+def multitest_same_num(num_infect, test_count):
+    infection_rates = []
+    for i in range(test_count):
+        people = []
+        for i in range(100):
+            people.append(Person())
+        infection_rates.append(simulate(people, num_infect))
+    for group in infection_rates:
+        plt.plot(group)
+
