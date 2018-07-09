7dfd2418f32826b836846c10a5c91cf5fdc8e224
Nicholas Gaydos <ngaydos@gmail.com>
2018-07-09 11:14:03 -0700

    added simulate to geo_spread

--- geo_spread.py
+++ geo_spread.py
@@ -19,6 +19,20 @@ class Grid:
             if person.yloc + ymove <= self.yarea[1] and person.yloc + ymove >= self.yarea[0]:
                 person.yloc += ymove
 
+    def simulate(self, infection_range, generation_count, movement_distance = 1):
+        random.choice(person).infected = True
+        for i in range(generation_count):
+            infected_people = [person if person.infected == True for person in self.people]
+            non_infected_people = [person if person.infected == False for person in self.people]
+            for _ in range(movement_distance):
+                self.all_move()
+            for infected_person in infected_people:
+                for non_infected_person in non_infected_people:
+                    if infected_person.distance_from(non_infected_person) <= infection_range:
+                        non_infected_person.infected == True
+        return sum([person.infected for person in self.people])
+
+
 class Person:
 
     def __init__(self, xloc, yloc, infected = False):
@@ -31,7 +45,7 @@ class Person:
         return math.sqrt(((self.xloc - other.xloc) ** 2 ) + ((self.yloc - other.yloc) **2))
 
 if __name__ == '__main__':
-    people = [Person(random.randint(0, 20), random.randint(0, 20))]
+    people = [Person(random.randint(0, 20), random.randint(0, 20)) ]
     grid = Grid(20, 20, people)
     xplaces = [people[0].xloc]
     yplaces = [people[0].yloc]
